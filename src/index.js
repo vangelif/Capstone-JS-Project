0f62dbf74f935931d60513049c832a083ea95b60
Vangelis <ev.fotiades@yahoo.com>
2023-03-21 20:49:25 +0200

    Include functionality for popup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+const commentsBtn = document.getElementById('comments');
+const closeBtn = document.getElementById('close-button');
+const popUp = document.getElementById('popup-window');
+
+commentsBtn.addEventListener('click', () => {
+  popUp.style.visibility = 'visible';
+});
+
+closeBtn.addEventListener('click', () => {
+  popUp.style.visibility = 'hidden';
+});
