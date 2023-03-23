c2008cc8fa592bdcda02b3e45549e72f025938b5
Vangelis <ev.fotiades@yahoo.com>
2023-03-23 14:39:57 +0200

    include the close button for the pop-up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './style.css';
 import { movieList } from './modules/homepage.js';
 import popup from './modules/popup.js';
+import modalClose from './modules/modalClose.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   await movieList();
   const cardContainer = document.querySelector('.card-container');
+  const xButton = document.getElementById('x-button');
+  const submitComment = document.getElementById('submit-button');
 
   cardContainer.addEventListener('click', async (event) => {
     if (event.target.matches('.comment-button')) {
@@ -12,4 +15,14 @@ document.addEventListener('DOMContentLoaded', async () => {
       popup(movieCard);
     }
   });
+
+  xButton.addEventListener('click', async () => {
+    modalClose();
+  });
+
+  // submitComment.addEventListener('click', async () => {
+  //   alert('i am here!!');
+  //   //create a function to get the input.value and textarea.value
+  //   //
+  // });
 });
